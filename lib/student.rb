c549e5c6cf22650afe4e1e859cc55a356433362f
Alex Kiborgok <borgokalek14@gmail.com>
2022-06-11 21:23:46 +0300

    commit tree 149ec2ed643151d51d3aaac59fef31c7a153a569

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+require_relative './user'
+class Student < User
+    attr_accessor :knowledge
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
 
+    def learn(new_knowledge)
+        @knowledge << new_knowledge
+    end
 end
